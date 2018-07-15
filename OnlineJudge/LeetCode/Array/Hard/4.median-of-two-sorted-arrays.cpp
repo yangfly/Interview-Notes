467b4f7a41ac765e54b46c9d711ae7936cb051ed
yangfly <1005912864@qq.com>
2018-07-16 01:16:08 +0800

    add LeetCode #4

--- OnlineJudge/LeetCode/Array/Hard/4.median-of-two-sorted-arrays.cpp
+++ OnlineJudge/LeetCode/Array/Hard/4.median-of-two-sorted-arrays.cpp
@@ -32,9 +32,40 @@
  * 
  * 
  */
+
+static const auto ________ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        
+        int m = nums1.size(), n = nums2.size();
+        int total = m + n;
+        if (total & 0x1)
+            return find_kth(nums1.begin(), m, nums2.begin(), n, (total + 1) / 2);
+        else
+            return (find_kth(nums1.begin(), m, nums2.begin(), n, total / 2)
+                   + find_kth(nums1.begin(), m, nums2.begin(), n, total / 2 + 1)) / 2.0;
+    }
+
+private:
+    int find_kth(vector<int>::const_iterator A, int m,
+                 vector<int>::const_iterator B, int n, int k) {
+        // always assume m <= n
+        if (m > n) return find_kth(B, n, A, m, k);
+        if (m == 0) return B[k - 1];
+        if (k == 1) return min(*A, *B);
+
+        // devide k into two parts
+        int im = min(k / 2, m), in = k - im;
+        if (A[im - 1] < B[in - 1])
+            return find_kth(A + im, m - im, B, n, k - im);
+        else if (A[im - 1] > B[in - 1])
+            return find_kth(A, m, B + in, n - in, k - in);
+        else
+            return A[im - 1];
     }
 };
---
+++ OnlineJudge/LeetCode/Array/summary.md
@@ -0,0 +1,2 @@
+### [4] [Median of Two Sorted Arrays](Hard/4.median-of-two-sorted-arrays.cpp)
+
